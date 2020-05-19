a475f4073e6a73b896427bd38ae1b4848bd0384b
Nikhil Keshav <nikhil.keshav@niser.ac.in>
2020-05-19 12:47:06 +0530

    Update weather_data.py
    Code makes html file of data webpage by city name
    Current issue: Race condition 
    Next step: extracting csv file from HTML using beautiful soup

--- weather_data.py
+++ weather_data.py
@@ -1,16 +1,23 @@
-from Selenium import webdriver
+from selenium import webdriver
+from bs4 import BeautifulSoup as soup
 import time
-from Selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
+
 print("sample test case started")
 driver = webdriver.Chrome()
 #navigate to the url
 driver.get("http://www.weatherbase.com/weather/city.php3?c=IN&name=India")
 #search city and click
+driver.implicitly_wait(15)
 driver.find_element_by_link_text("Bangalore").click()
+driver.implicitly_wait(15)
 #Show all data of the city
 driver.find_element_by_link_text("Show All Data").click()
-time.sleep(2)
-
-#close the browser
+#driver.find_element_by_link_text("Monthly - All Data").click()
+html = driver.page_source
+soup = soup(html, features="html.parser")
+f = open("Bangalore.html", "w", encoding="utf-8")
+f.write(str(soup))
+f.close()
 driver.close()
 print("sample test case successfully completed")
